402105a37366afe4b5bc18443c061eca962fc1db
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2017-07-03 20:18:12 +0000

    Update docs after building Travis build 52 of choldgraf/download
    The docs were built from the branch 'master' against the commit
    4b0375f050bdfe95be85137bd022e8561a5b1822.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/choldgraf/download/jobs/249765704.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.1/bin/doctr deploy . --built-docs doc/_build/html

--- _downloads/plot_download_providers.py
+++ _downloads/plot_download_providers.py
@@ -39,8 +39,8 @@ ax.set_axis_off()
 
 ###############################################################################
 # Dropbox links also work, though in this case ``download`` will use
-# the ``wget`` library to download the file. This is because Dropbox requires
-# cookies and wget is smart about handling this.
+# the ``requests`` library to download the file. This is because Dropbox
+# requires cookies and requests is smart about handling this.
 
 url = "https://www.dropbox.com/s/fow6bx4owwikjug/citation.png?dl=0"
 path3 = download(url, './downloaded/citation3.png', replace=True)
