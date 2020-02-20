b8a5a3da8b5940dbd26794f5d4f1f571e01beb73
DanielDevBR <ferion11@gmail.com>
2020-02-20 17:14:59 -0300

    Update download/download.py
    Co-Authored-By: Chris Holdgraf <choldgraf@gmail.com>

--- download/download.py
+++ download/download.py
@@ -412,6 +412,7 @@ def request_agent(url):
     req = urllib.request.Request(
                 url,
                 data = None,
+                # Simulate a user-agent because some websites require it for this to work
                 headers = {
                     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
                 }
